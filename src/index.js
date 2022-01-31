5fd4fbb30f7eac24fe7ab69966b22f38fa4954c8
SumaiaSorna <sorna.sumaia@gmail.com>
2022-01-31 13:50:06 +0000

    fixed bug in the login and signup

--- src/index.js
+++ src/index.js
@@ -45,9 +45,7 @@ const init = async () => {
     await connection.sync({ force: false });
     console.log("[INFO]: DB connection successful");
 
-    app.listen(PORT, () =>
-      console.log(`🚀🚀 Navigate to http://localhost:${PORT}`)
-    );
+    app.listen(PORT, () => console.log(`Navigate to http://localhost:${PORT}`));
   } catch (error) {
     console.log(`[ERROR]: DB connection failed | ${error.message}`);
   }
